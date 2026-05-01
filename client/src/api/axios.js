05fe9bb0aba2c7fbb9a456236425a38b6c0ad533
Gulshan Kumar <gulshankumar725044@gmail.com>
2026-05-01 13:23:03 +0530

    added env in client side

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "teamtask-production-5d86.up.railway.app"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
